7cd160e248cc8c2762ccd3ee31e833c94204acbe
黎伟航 <lwh1094464365@163.com>
2020-07-19 14:50:06 +0800

    添加小听详情页面

--- app/src/main/java/com/shuyue/snack/activity/DetailActivity.java
+++ app/src/main/java/com/shuyue/snack/activity/DetailActivity.java
@@ -3,11 +3,16 @@ package com.shuyue.snack.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,6 +54,8 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        fullScreen(this);
+
         ButterKnife.bind(this);
 
         Snack snack = (Snack) getIntent().getSerializableExtra("snack");
@@ -72,6 +79,26 @@ public class DetailActivity extends AppCompatActivity {
                     Tips.show("已在购物车中，不能重复添加");
                 }
             });
+        }
+    }
+
+    /**
+     * 通过设置全屏，设置状态栏透明
+     */
+    private void fullScreen(Activity activity) {
+        Window window = activity.getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = ((Window) window).getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else {
+            WindowManager.LayoutParams attributes = window.getAttributes();
+            int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            attributes.flags |= flagTranslucentStatus;
+            window.setAttributes(attributes);
         }
     }
 }
