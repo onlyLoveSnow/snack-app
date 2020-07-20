19410748b92aba314afa47f9ec7cc47eb37f7e38
黎伟航 <lwh1094464365@163.com>
2020-07-20 15:06:36 +0800

    添加登录页面并实现本地验证和持久化登录状态

--- app/src/main/java/com/shuyue/snack/MyApplication.java
+++ app/src/main/java/com/shuyue/snack/MyApplication.java
@@ -3,6 +3,7 @@ package com.shuyue.snack;
 import android.app.Application;
 
 import com.shuyue.snack.model.Snack;
+import com.shuyue.snack.model.User;
 import com.shuyue.snack.utils.Utils;
 
 import java.util.ArrayList;
@@ -20,12 +21,43 @@ public class MyApplication extends Application {
      */
     private static List<Snack> cartSnacks;
 
+    /**
+     * 登录用户
+     */
+    private static User user;
+
+    /**
+     * 登录状态
+     */
+    private static boolean isLogin;
+
     public static MyApplication getInstance() {
         return appContext;
     }
 
     public static List<Snack> getCartSnacks() {
         return cartSnacks;
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        MyApplication.user = user;
+    }
+
+    /**
+     * 是否登录
+     *
+     * @return <tt>true</tt>: 已经登录, <tt>false</tt>: 未登录
+     */
+    public static boolean isLogin() {
+        return isLogin;
+    }
+
+    public static void isLogin(boolean isLogin) {
+        MyApplication.isLogin = isLogin;
     }
 
     @Override
