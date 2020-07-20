65d3e13f8afdcbd9684d82d5db3edd9207fda6e2
黎伟航 <lwh1094464365@163.com>
2020-07-20 15:06:58 +0800

    添加登录页面并实现本地验证和持久化登录状态

--- app/src/main/java/com/shuyue/snack/model/User.java
+++ app/src/main/java/com/shuyue/snack/model/User.java
@@ -3,6 +3,7 @@ package com.shuyue.snack.model;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class User implements Serializable {
 
@@ -16,7 +17,7 @@ public class User implements Serializable {
 
     private String nickname;
 
-    private String headImage;
+    private int headImage;
 
     public User() {
     }
@@ -26,7 +27,7 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public User(String username, String password, String nickname, String headImage) {
+    public User(String username, String password, String nickname, int headImage) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
@@ -65,12 +66,32 @@ public class User implements Serializable {
         this.nickname = nickname;
     }
 
-    public String getHeadImage() {
+    public int getHeadImage() {
         return headImage;
     }
 
-    public void setHeadImage(String headImage) {
+    public void setHeadImage(int headImage) {
         this.headImage = headImage;
+    }
+
+    /**
+     * 用于登录比较用户账号密码
+     *
+     * @param o 待比较用户
+     * @return 比较结果
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
     }
 
     @NotNull
